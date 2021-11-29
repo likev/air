0423882dd5e2f0a00353288da67c73351cf634d0
likev <36227022+read3@users.noreply.github.com>
2021-11-29 16:58:56 +0800

    new hnzdb image 024.png

--- libs/zdb-img.php
+++ libs/zdb-img.php
@@ -6,7 +6,7 @@ $filename = $_GET['filename'];				//获取提交的action值
 
 if($filename)
 {
-	header('Content-type: image/jpeg');
+	header('Content-type: image/png');
 	$ftpUrl = 'ftp://dspddown:dspd1224@172.18.152.5/forecast/dqyb/kqwr-zdb/province/' . $filename;
 	echo file_get_contents($ftpUrl);
 }
